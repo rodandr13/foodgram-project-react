59523d61ce63e06c7c347f8be18f6d8cd5e569b6
Andrei Roditelev <rodandr13@gmail.com>
2022-08-25 14:02:04 +0600

    basic viewsets, serializers, routers for Api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import TagViewSet, IngredientViewSet, RecipeViewSet, UserViewSet
 
 app_name = 'api'
 
+router = DefaultRouter()
+router.register('tags', TagViewSet)
+router.register('ingredients', IngredientViewSet)
+router.register('recipes', RecipeViewSet)
+router.register('users', UserViewSet)
+
 urlpatterns = [
-    path('', include('djoser.urls')),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-# Create your views here.
+from recipes.models import Ingredient, Tag, Recipe, IngredientInRecipe
+from .serializers import TagSerializer, IngredientSerializer, UserSerializer, RecipeSerializer
+
+
+User = get_user_model()
+
+
+class TagViewSet(ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = UserSerializer
+
+
+class RecipeViewSet(ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = RecipeSerializer
