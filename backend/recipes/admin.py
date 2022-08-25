00346916efd115f5d4b3012897efcf7fba5c3df5
Andrei Roditelev <rodandr13@gmail.com>
2022-08-25 20:56:12 +0600

    created an admin panel for recipes

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import IngredientInRecipe, Tag, Recipe, Ingredient
+
+EMPTY_MESSAGE = '-пусто-'
+
+
+class IngredientInRecipeAdmin(admin.TabularInline):
+    model = IngredientInRecipe
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+    save_on_top = True
+    empty_value_display = EMPTY_MESSAGE
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author',)
+    search_fields = ('author', 'name', 'tags',)
+    empty_value_display = EMPTY_MESSAGE
+    inlines = (IngredientInRecipeAdmin,)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug',)
+    search_fields = ('name', 'color',)
+    save_on_top = True
+    empty_value_display = EMPTY_MESSAGE
